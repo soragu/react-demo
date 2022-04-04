b4b4e575d8fd86c0e960d248bb29be97d65d92bd
soragu <soragu@163.com>
2022-04-04 10:07:28 +0800

    Add match path for nav item style controlling

--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -11,12 +11,21 @@ import {
 } from '@mui/material'
 import { NavLink, matchPath, useLocation } from 'react-router-dom'
 
+const ativeStyle = {
+  fontWeight: 600,
+  color: '#000',
+  background: '#a5b4d0'
+}
+
 function NavItem(props) {
   const {path, icon, title} = props
+  const { pathname } = useLocation()
+  const isActive = matchPath({ path, end: false }, pathname)
   return (
     <ListItem
       component={NavLink}
       to={path}
+      style= {() => isActive ? ativeStyle : {}}
     >
       <ListItemIcon>{icon && icon}</ListItemIcon>
       <ListItemText disableTypography primary={title} />
@@ -24,13 +33,17 @@ function NavItem(props) {
   )
 }
 
+
 function SideBar() {
   return (
     <Box sx={{ width: '100%', height: '100%', background: '#e8f0ff'}}>
       <List component="nav">
         {
           sidebarConfig.map((item) => (
-            <NavItem key={item.path} {...item} />
+            <NavItem
+              key={item.path}
+              {...item}
+            />
           ))
         }
       </List>
